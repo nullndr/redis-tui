cc165f4ae400cf8373601276f2627334937911d5
Andrea <andrea@yaaaw.it>
2022-08-07 22:26:37 +0200

    feat: add basic terminal structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-  println!("Hello, world!");
+use crossterm::event::{poll, read, Event, KeyCode};
+use std::{io, time::Duration};
+use tui::Terminal;
+
+mod utils;
+
+use utils::utils::{create_backend, create_primary_block, restore_terminal};
+
+fn main() -> Result<(), io::Error> {
+  if let Ok(backend) = create_backend() {
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+      let size = f.size();
+      let block = create_primary_block("Redis TUI");
+      f.render_widget(block, size);
+    })?;
+
+    loop {
+      if poll(Duration::from_millis(500))? {
+        match read()? {
+          Event::Key(event) => {
+            if event.code == KeyCode::Char('q') {
+              if let Ok(_) = restore_terminal(&mut terminal) {
+                return Ok(());
+              } else {
+                panic!("Failed to restore terminal");
+              }
+            } else {
+              continue;
+            }
+          }
+          _ => continue,
+        }
+      }
+    }
+  } else {
+    Err(io::Error::new(
+      io::ErrorKind::Other,
+      "Failed to create backend",
+    ))
+  }
 }
